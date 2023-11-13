ae38d865825c9b224a546112a4b511ce1c9ee6d3
Vengatesan K <128688827+Vengatesan-K@users.noreply.github.com>
2023-11-13 18:48:29 +0530

    Add files via upload

--- Retail.py
+++ Retail.py
@@ -16,6 +16,15 @@ from sklearn.preprocessing import OneHotEncoder
 import plotly.graph_objs as go
 st.set_page_config(page_title='Retail', layout='wide', page_icon="🛒")
 import plotly.figure_factory as ff
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+from prophet.plot import plot_plotly, plot_components_plotly
+from prophet import Prophet
+from prophet.diagnostics import cross_validation, performance_metrics
+import warnings
+warnings.filterwarnings('ignore')
+
 st.write("""
 <div style='text-align:right'>
     <h1 style='color:#1e90ff;'>🛒Retail Sales Forecasting</h1>
@@ -96,7 +105,7 @@ fig.update_layout(
 )
 
 st.plotly_chart(fig, use_container_width=True)
-
+st.markdown("---")
 average_sales_per_week = df.groupby(by=['Date'], as_index=False)['Weekly_Sales'].sum()
 average_sales_per_week= average_sales_per_week.sort_values('Date', ascending=True)
 fig1 = px.line(average_sales_per_week, x='Date', y='Weekly_Sales', title='Total Weekly Sales Over Time',markers=True)
@@ -106,9 +115,43 @@ fig1.update_layout(
     xaxis_title='Date',
     yaxis_title='Total Weekly Sales',title_x=0.45,height=400
 )
-
+fig1.add_annotation(
+    text='Highest Sales : $56.71M(Dec-24-2010)',
+    x='2010-12-24',  
+    y=56710000,
+    arrowhead=2,
+    arrowsize=1.5,
+    arrowwidth=2,
+    arrowcolor='red',
+    bgcolor='rgba(255, 255, 255, 0.7)',
+    opacity=0.8
+)
 # Use Streamlit to display the Plotly figure
 st.plotly_chart(fig1, use_container_width=True)
+st.markdown("---")
+
+average_sales_per_week_n = df.groupby(by=['Date','IsHoliday'], as_index=False)['Weekly_Sales'].sum()
+colors = {0: 'red', 1: 'green'}
+fig9 = px.line(
+    average_sales_per_week_n,
+    x="Date",
+    y="Weekly_Sales",
+    color="IsHoliday",color_discrete_map=colors,
+    labels={"Weekly_Sales": "Weekly Sales", "Date": "Date"},
+    title="Weekly Sales with Holidays",
+)
+
+# Update figure height and title position
+fig9.update_layout(
+    height=400,
+    title=dict(
+        x=0.45
+    )
+)
+
+st.plotly_chart(fig9,use_container_width=True)
+st.markdown("---")
+
 
 average_sales_per_date = df.loc[:, ('Date','Weekly_Sales')]
 average_sales_per_date['Month'] =pd.DatetimeIndex(average_sales_per_date['Date']).month
@@ -122,10 +165,20 @@ fig2.update_layout(
     xaxis_title='Month',
     yaxis_title='Total Weekly Sales',title_x=0.45,height=400
 )
-
+fig2.add_annotation(
+    text='Highest Sales : $551M(April)',
+    x=4,  
+    y=551998331,
+    arrowhead=2,
+    arrowsize=1.5,
+    arrowwidth=2,
+    arrowcolor='red',
+    bgcolor='rgba(255, 255, 255, 0.7)',
+    opacity=0.8
+)
 # Use Streamlit to display the Plotly figure
 st.plotly_chart(fig2, use_container_width=True)
-
+st.markdown("---")
 average_sales_per_year = average_sales_per_date.groupby(by=['Year'], as_index=False)['Weekly_Sales'].sum()
 
 fig3 = px.bar(average_sales_per_year, x='Year', y='Weekly_Sales', title='Total Weekly Sales per Year',color='Weekly_Sales',
@@ -135,10 +188,20 @@ fig3.update_layout(
     xaxis_title='Year',
     yaxis_title='Total Weekly Sales',title_x=0.45,height=400
 )
-
+fig3.add_annotation(
+    text='Highest Sales : $2B(2011)',
+    x=2011,  
+    y=2069889649,
+    arrowhead=2,
+    arrowsize=1.5,
+    arrowwidth=2,
+    arrowcolor='red',
+    bgcolor='rgba(255, 255, 255, 0.7)',
+    opacity=0.8
+)
 # Use Streamlit to display the Plotly figure
 st.plotly_chart(fig3, use_container_width=True)
-
+st.markdown("---")
 sales_per_store_type = df.groupby(by=['Type'], as_index=False)['Weekly_Sales'].sum()
 sales_per_store_type_n = df.groupby(by=['Date','Type'], as_index=False)['Weekly_Sales'].sum()
 
@@ -151,10 +214,20 @@ fig4.update_layout(
     xaxis_title='Date',
     yaxis_title='Total Weekly Sales',title_x=0.45,height=400
 )
-
+fig4.add_annotation(
+    text='Best Sales over type : A',
+    x='2012-09-14',  
+    y=25000000,
+    arrowhead=2,
+    arrowsize=1.5,
+    arrowwidth=2,
+    arrowcolor='blue',
+    bgcolor='rgba(255, 255, 255, 0.7)',
+    opacity=0.8
+)
 # Use Streamlit to display the Plotly figure
 st.plotly_chart(fig4, use_container_width=True)
-
+st.markdown("---")
 sales_by_dept = df.groupby(by=['Dept'], as_index=False)['Weekly_Sales'].sum()
 
 fig5 = go.Figure()
@@ -175,10 +248,20 @@ fig5.update_layout(
     xaxis_title='Department',
     yaxis_title='Sales',title_x=0.45,height=400
 )
-
+fig5.add_annotation(
+    text='Highest Sales - 267.50M(Dept-76)',
+    x=76,  
+    y=279508700,
+    arrowhead=2,
+    arrowsize=1.5,
+    arrowwidth=2,
+    arrowcolor='green',
+    bgcolor='rgba(255, 255, 255, 0.7)',
+    opacity=0.8
+)
 # Use Streamlit to display the Plotly figure
 st.plotly_chart(fig5, use_container_width=True)
-
+st.markdown("---")
 train_markdown = df[df.MarkDown2.notnull()]
 train_markdown = train_markdown.groupby("Date").agg({"MarkDown1":"mean","MarkDown2":"mean","MarkDown3":"mean","MarkDown4":"mean","MarkDown5":"mean"})
 fig6 = make_subplots(rows=5, cols=1, shared_xaxes=True, subplot_titles=['Markdown1', 'Markdown2', 'Markdown3', 'Markdown4', 'Markdown5'])
@@ -197,7 +280,7 @@ fig6.update_layout(
 
 # Use Streamlit to display the Plotly figure
 st.plotly_chart(fig6, use_container_width=True)
-
+st.markdown("---")
 
 train_markdown.index = pd.to_datetime(train_markdown.index)
 
@@ -224,7 +307,7 @@ fig7.update_layout(
 
 # Use Streamlit to display the Plotly figure
 st.plotly_chart(fig7, use_container_width=True)
-
+st.markdown("---")
 selected_columns = ['Store', 'Temperature', 'Fuel_Price', 'MarkDown1', 'MarkDown2', 'MarkDown3', 'MarkDown4',
                      'MarkDown5', 'CPI', 'Unemployment', 'IsHoliday']
 
@@ -247,10 +330,190 @@ fig8.update_layout(
     height=500, 
     width=500,title_x=0.40
 )
+
+
 # Use Streamlit to display the Plotly figure
 st.plotly_chart(fig8,use_container_width=True)
+high_corr_threshold = 0.9  # You can adjust this threshold based on your preference
+
+high_corr_text = ""
+for i, column in enumerate(selected_columns):
+    for j, row in enumerate(selected_columns):
+        correlation_value = corr_matrix.iloc[i, j]
+        if abs(correlation_value) > high_corr_threshold and i != j:
+            high_corr_text += f"<li>({column} and {row} : {correlation_value:.2f})</li>"
+
+import streamlit as st
+
+# Your high correlation text
+high_corr_text = """
+    <ul>
+        <li>The retail weekly sales experience heightened levels during December and February, primarily attributable to increased holiday shopping activity and high markdowns (discounted prices)</li>
+        <li>Weekly sales of stores of bigger sizes are generally higher than stores of smaller sizes. Stores of type A are the largest, followed by B and then C being the smallest size stores. However, the minimum weekly sales of store B are higher than that of store A.</li>
+        <li>🧮 High Correlation Columns : {high_corr_text} </li>
+        <!-- Add more items as needed -->
+    </ul>
+"""
+
+# Custom CSS styling
+custom_css = """
+    <style>
+        .high-corr-container {
+            padding: 10px;
+            background-color: #f0f0f0;
+            border-radius: 5px;
+            margin-top: 20px;
+        }
+
+        .bullet-list {
+            list-style-type: disc;
+            padding-left: 20px;
+        }
+    </style>
+"""
+
+# Display the custom-styled content
+st.markdown(f"{custom_css}<div class='high-corr-container'><h4>💡 Insights:</h4><ul class='bullet-list'>{high_corr_text}</ul></div>", unsafe_allow_html=True)
+st.markdown("---")
 
 
+df.Date = pd.to_datetime(df.Date)
+store_1 = df[(df.Store == 1) & (df.Dept == 1)].sort_values('Date')
+df_1 = store_1[['Date', 'Weekly_Sales']]
+df_1.columns = ['ds', 'y']
+m = Prophet(interval_width=.95, 
+            daily_seasonality=False, yearly_seasonality=True,
+            weekly_seasonality=True,seasonality_mode='multiplicative').fit(df_1)
+future = m.make_future_dataframe(periods=10, freq='W')
+forecast = m.predict(future)
+
+fig9 = px.line(forecast, x='ds', y=['yhat'], title='Prophet Forecast')
+fig9.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat_lower'], fill='tonexty', mode='lines', line=dict(color='rgba(0,100,80,0.2)'), name='Lower Bound'))
+fig9.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat_upper'], fill='tonexty', mode='lines', line=dict(color='rgba(0,100,80,0.2)'), name='Upper Bound'))
+
+st.plotly_chart(fig9,use_container_width=True)
+import plotly.subplots as sp
+fig_components = sp.make_subplots(rows=3, cols=1, subplot_titles=('Trend', 'Weekly', 'Yearly'))
+
+# Plotting components
+fig_components.add_trace(go.Scatter(x=forecast['ds'], y=forecast['trend'], mode='lines', name='Trend'), row=1, col=1)
+fig_components.add_trace(go.Scatter(x=forecast['ds'], y=forecast['weekly'], mode='lines', name='Weekly'), row=2, col=1)
+fig_components.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yearly'], mode='lines', name='Yearly'), row=3, col=1)
+
+# Customize layout
+fig_components.update_layout(title_text='Prophet Components', height=800)
+
+# Display the Plotly figure in Streamlit
+st.plotly_chart(fig_components,use_container_width=True)
+
+import os
+
+class suppress_stdout_stderr(object):
+
+    def __init__(self):
+        self.null_fds = [os.open(os.devnull, os.O_RDWR) for x in range(2)]
+        self.save_fds = (os.dup(1), os.dup(2))
+
+    def __enter__(self):
+        os.dup2(self.null_fds[0], 1)
+        os.dup2(self.null_fds[1], 2)
+
+    def __exit__(self, *_):
+        os.dup2(self.save_fds[0], 1)
+        os.dup2(self.save_fds[1], 2)
+        os.close(self.null_fds[0])
+        os.close(self.null_fds[1])
+def getCrossValidationData(m):
+    with suppress_stdout_stderr():
+        c_v = cross_validation(m, 
+                               initial='120W',   # Initially, the model will be trained in 120 weeks.
+                               period='2W',      # After each model tested, we'll add 2 more weeks.
+                               horizon ='2W',    # The forecasting will happen in a range of 2 weeks.
+                               parallel="processes",   # To acellerate the cross-validation.
+                              )
+    return c_v
+
+def getPerfomanceMetrics(m):
+    return performance_metrics(getCrossValidationData(m), 
+                               rolling_window=1, # Generate metrics for the whole (100%) seen data.
+                              )
+    
+superbowls = pd.DataFrame({
+  'holiday': 'superbowl',
+  'ds': pd.to_datetime(['2010-02-07', '2011-02-06', '2012-02-05', '2013-02-03']),
+  'lower_window': -2,
+  'upper_window': 2,
+})
+easter = pd.DataFrame({
+  'holiday': 'easter',
+  'ds': pd.to_datetime(['2010-04-05', '2011-04-25', '2012-04-09', '2013-04-01']),
+  'lower_window': -2,
+  'upper_window': 1,
+})
+mothers_day = pd.DataFrame({
+    'holiday': "mother's day",
+    'ds': pd.to_datetime(['2010-05-09', '2011-05-08', '2012-05-13', '2013-02-12']),
+    'lower_window': -3,
+    'upper_window': 0,
+})
+fathers_day = pd.DataFrame({
+    'holiday': "father's day",
+    'ds': pd.to_datetime(['2010-06-19', '2011-06-19', '2012-06-17', '2013-06-16']),
+    'lower_window': -3,
+    'upper_window': 0,
+})
+halloween = pd.DataFrame({
+    'holiday': "father's day",
+    'ds': pd.to_datetime(['2010-10-31', '2011-10-31', '2012-10-31', '2013-10-31']),
+    'lower_window': -3,
+    'upper_window': 2,
+})
+black_friday = pd.DataFrame({
+    'holiday': "black friday",
+    'ds': pd.to_datetime(['2010-11-26', '2011-11-25', '2012-11-23', '2013-11-29']),
+    'lower_window': 0,
+    'upper_window': 0,
+})
+cyber_monday = pd.DataFrame({
+    'holiday': "cyber monday",
+    'ds': pd.to_datetime(['2010-11-29', '2011-11-28', '2012-12-26', '2013-12-02']),
+    'lower_window': 0,
+    'upper_window': 0,
+})
+
+holidays = pd.concat((superbowls, 
+                      easter,
+                      mothers_day, 
+                      fathers_day, 
+                      halloween, 
+                      black_friday, 
+                      cyber_monday))
+
+
+m = Prophet(holidays=holidays,
+            interval_width=.95, 
+            daily_seasonality=False)
+m.add_country_holidays(country_name='US')
+with suppress_stdout_stderr():
+    m.fit(df_1)
+    
+end_date = st.date_input('Select the end date for forecasting:', min_value=df_1['ds'].max(), max_value=pd.Timestamp.now())
+
+future = pd.DataFrame({'ds': pd.date_range(start=df_1['ds'].min(), end=end_date, freq='W')})
+
+forecast = m.predict(future)
+
+fig_forecast = px.line(forecast, x='ds', y=['yhat'], title=f'Prophet Forecast (Until {end_date})')
+fig_forecast.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat_lower'], fill='tonexty', mode='lines', line=dict(color='rgba(0,100,80,0.2)'), name='Lower Bound'))
+fig_forecast.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat_upper'], fill='tonexty', mode='lines', line=dict(color='rgba(0,100,80,0.2)'), name='Upper Bound'))
+
+st.plotly_chart(fig_forecast,use_container_width=True)
+
+
+
+
+
+#st.markdown("<hr>", unsafe_allow_html=True)
 def style_metric_cards(
     background_color: str = "#FFF",
     border_size_px: int = 1,
@@ -301,8 +564,10 @@ st.markdown("""
         <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#predict-price">Price</a>
+        <a class="nav-link" href="#analysis-of-historical-retail-data">Analysis</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#predict-week-sales">Forecasting Sales</a>
     </ul>
   </div>
 </nav>
